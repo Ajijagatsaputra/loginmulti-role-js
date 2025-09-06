c03e486f76880e526c92581901ad99bf2d0ae998
Ajijagatsaputra <ajijagat90@gmail.com>
2025-09-06 19:41:37 +0700

    add frontend

---
+++ frontend/src/pages/Dashboard.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import Layout from "./Layout";
+
+const Dashboard = () => {
+  return (
+    <Layout>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Expedita
+      veritatis cum ducimus nesciunt. Quas quo accusamus ipsa eligendi officiis.
+      Hic eligendi dolore maiores officiis cum laboriosam placeat vitae cumque
+      nam.
+    </Layout>
+  );
+};
+
+export default Dashboard;
